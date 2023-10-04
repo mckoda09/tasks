000951902d5c59a340383dc0479beeaac22ab83e
mckoda09 <mckoda09@gmail.com>
2023-10-04 18:31:06 +0300

    made template

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Home() {
   return (
-    <main></main>
+    <main className="flex items-center justify-center h-[calc(100dvh)] w-full">
+      <Card>
+        <CardHeader>
+          <CardTitle>Выйди и войди нармально</CardTitle>
+          <CardDescription>чел ну ты это</CardDescription>
+        </CardHeader>
+        <CardContent>мда треш пон кнопок нет и чё а ты не парься ливни отсюда</CardContent>
+      </Card>
+    </main>
   )
 }
